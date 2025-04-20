685bb2859a8efc263b1a09b62980bdbe880261e9
prajwalbayari <prajwalbayari4@gmail.com>
2025-04-20 23:32:23 +0530

    Routing pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,54 @@
-import React from "react";
-import { Routes , Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
 import AuthPage from "./pages/auth";
+import RouteGuard from "./components/route-guard";
+import { AuthContext } from "./context/auth-context";
+import InstructorDashboardPage from "./pages/instructor";
+import StudentViewCommonLayout from "./components/student-view/common-layout";
+import StudentHomePage from "./pages/student/home";
+import NotFoundPage from "./pages/not-found";
 
 const App = () => {
-  return(
+  const { auth } = useContext(AuthContext);
+  return (
     <Routes>
-      <Route path="/auth" element={<AuthPage/>} />
+      <Route
+        path="/auth"
+        element={
+          <RouteGuard
+            element={<AuthPage />}
+            authenticated={auth?.authenticated}
+            user={auth?.user}
+          />
+        }
+      />
+
+      <Route
+        path="/instructor"
+        element={
+          <RouteGuard
+            element={<InstructorDashboardPage />}
+            authenticated={auth?.authenticated}
+            user={auth?.user}
+          />
+        }
+      />
+
+      <Route
+        path="/"
+        element={
+          <RouteGuard
+            element={<StudentViewCommonLayout />}
+            authenticated={auth?.authenticated}
+            user={auth?.user}
+          />
+        }
+      >
+        <Route path="" element={<StudentHomePage />}></Route>
+        <Route path="home" element={<StudentHomePage />}></Route>
+      </Route>
+
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 };
---
+++ client/src/components/route-guard/index.jsx
@@ -0,0 +1,30 @@
+import { Fragment } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+function RouteGuard({ authenticated, user, element }) {
+  const location = useLocation();
+  if (!authenticated && !location.pathname.includes("/auth")) {
+    return <Navigate to={"/auth"} />;
+  }
+
+  if (
+    authenticated &&
+    user?.role !== "instructor" &&
+    (location.pathname.includes("/instructor") ||
+      location.pathname.includes("auth"))
+  ) {
+    return <Navigate to={"/home"} />;
+  }
+
+  if (
+    authenticated &&
+    user?.role === "instructor" &&
+    !location.pathname.includes("/instructor")
+  ) {
+    return <Navigate to={"/instructor"} />;
+  }
+
+  return <Fragment>{element}</Fragment>;
+}
+
+export default RouteGuard;
