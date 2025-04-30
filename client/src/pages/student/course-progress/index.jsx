5320cf746eb2bc081b7ec08463c31b3372e6503c
prajwalbayari <prajwalbayari4@gmail.com>
2025-04-30 15:03:43 +0530

    Basic structure of Course progress page

--- client/src/pages/student/course-progress/index.jsx
+++ client/src/pages/student/course-progress/index.jsx
@@ -7,10 +7,13 @@ import {
   DialogTitle,
 } from "@/components/ui/dialog";
 import { Label } from "@/components/ui/label";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import VideoPlayer from "@/components/video-player";
 import { AuthContext } from "@/context/auth-context";
 import { StudentContext } from "@/context/student-context";
 import { getCurrentCourseProgressService } from "@/services";
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { useContext, useEffect, useState } from "react";
 import Confetti from "react-confetti";
 import { useNavigate, useParams } from "react-router-dom";
@@ -26,6 +29,7 @@ function StudentViewCourseProgressPage() {
   const [showCourseCompleteDialog, setShowCourseCompleteDialog] =
     useState(false);
   const [showConfetti, setShowConfetti] = useState(false);
+  const [isSideBarOpen, setIsSideBarOpen] = useState(false);
 
   async function fetchCurrentCourseProgress(params) {
     const response = await getCurrentCourseProgressService(auth?.user?._id, id);
@@ -45,6 +49,11 @@ function StudentViewCourseProgressPage() {
           setShowConfetti(true);
           return;
         }
+
+        if (response?.data?.progress) {
+          setCurrentLecture(response?.data?.courseDetails?.curriculum[0]);
+        } else {
+        }
       }
     }
   }
@@ -58,6 +67,8 @@ function StudentViewCourseProgressPage() {
       setTimeout(() => setShowConfetti(false), 5000);
     }
   }, [showConfetti]);
+
+  console.log(currentLecture);
 
   return (
     <div className="flex flex-col h-screen bg-[#1c1d1f] text-white">
@@ -73,9 +84,84 @@ function StudentViewCourseProgressPage() {
             <ChevronLeft className="h-4 w-4 mr-2" />
             Back to My Courses Page
           </Button>
-          <h1></h1>
+          <h1 className="text-lg font-bold hidden md:block">
+            {studentCurrentCourseProgress?.courseDetails?.title}
+          </h1>
+        </div>
+        <Button onClick={() => setIsSideBarOpen(!isSideBarOpen)}>
+          {isSideBarOpen ? (
+            <ChevronRight className="h-5 w-5" />
+          ) : (
+            <ChevronLeft className="h-5 w-5" />
+          )}
+        </Button>
+      </div>
+
+      <div className="flex flex-1 overflow-hidden">
+        <div
+          className={`flex-1 ${
+            isSideBarOpen ? "mr-[400px]" : ""
+          } transition-all duration-300`}
+        >
+          <VideoPlayer
+            width="100%"
+            height="500px"
+            url={currentLecture?.videoURL}
+          />
+          <div className="p-6 bg-[#1c1d1f]">
+            <h2 className="font-bold text-2xl mb-2">{currentLecture?.title}</h2>
+          </div>
+        </div>
+        <div
+          className={`fixed top-[64px] right-0 bottom-0 w-[400px] bg-[#1c1d1f] border-l border-gray-700 transition-all duration-300 ${
+            isSideBarOpen ? "translate-x-0" : "translate-x-full"
+          }`}
+        >
+          <Tabs defaultValue="content" className="h-full flex flex-col">
+            <TabsList className="grid bg-[#1c1d1f] w-full grid-cols-2 p-0 h-14">
+              <TabsTrigger
+                value="content"
+                className="data-[state=active]:bg-white text-white rounded-none h-full"
+              >
+                Course Content
+              </TabsTrigger>
+              <TabsTrigger
+                value="overview"
+                className="data-[state=active]:bg-white text-white rounded-none h-full"
+              >
+                Overview
+              </TabsTrigger>
+            </TabsList>
+            <TabsContent value="content">
+              <ScrollArea className="h-full">
+                <div className="p-4 space-y-4">
+                  {studentCurrentCourseProgress?.courseDetails?.curriculum.map(
+                    (item) => (
+                      <div
+                        key={item._id}
+                        className="flex items-center space-x-2 text-sm text-white font-bold cursor-pointer"
+                      >
+                        <span>{item?.title}</span>
+                      </div>
+                    )
+                  )}
+                </div>
+              </ScrollArea>
+            </TabsContent>
+            <TabsContent value="overview" className="flex-1 overflow-hidden">
+              <ScrollArea className="h-full">
+                <div className="p-4">
+                  <h2 className="text-xl font-bold mb-4">About this course</h2>
+                  <p className="text-gray-400">
+                    {studentCurrentCourseProgress?.courseDetails?.description}
+                  </p>
+                </div>
+              </ScrollArea>
+            </TabsContent>
+          </Tabs>
         </div>
       </div>
+
       <Dialog open={lockCourse}>
         <DialogContent className="sm:w-[425px]">
           <DialogHeader>
