5fd9f3d75347b3b9500dea97f9a9e5f704c047c7
prajwalbayari <prajwalbayari4@gmail.com>
2025-07-31 12:11:20 +0530

    Fixed the update anamoly

--- server/controllers/instructor-controller/course-controller.js
+++ server/controllers/instructor-controller/course-controller.js
@@ -1,4 +1,5 @@
 import Course from "../../models/Course.js";
+import StudentCourses from "../../models/StudentCourses.js";
 
 const addNewCourse = async (req, res) => {
   try {
@@ -64,18 +65,40 @@ const updateCourseById = async (req, res) => {
   try {
     const { id } = req.params;
     const updatedCourseData = req.body;
-    const updatedCourse = await Course.findByIdAndUpdate(
-      id,
-      updatedCourseData,
-      { new: true }
-    );
-
-    if (!updatedCourse) {
-      res.status(404).json({
+    
+    // Get the existing course to preserve student enrollment data
+    const existingCourse = await Course.findById(id);
+    if (!existingCourse) {
+      return res.status(404).json({
         success: false,
         message: "Course not found!",
       });
     }
+
+    // Remove students field from update data to preserve existing enrollments
+    const { students, ...courseUpdateFields } = updatedCourseData;
+    
+    // Update course while preserving students array
+    const updatedCourse = await Course.findByIdAndUpdate(
+      id,
+      {
+        ...courseUpdateFields,
+        students: existingCourse.students, // Preserve existing student enrollment data
+      },
+      { new: true }
+    );
+
+    // Update course details in StudentCourses model for all students who purchased this course
+    await StudentCourses.updateMany(
+      { "courses.courseId": id },
+      {
+        $set: {
+          "courses.$.title": updatedCourse.title,
+          "courses.$.instructorName": updatedCourse.instructorName,
+          "courses.$.courseImage": updatedCourse.image,
+        }
+      }
+    );
 
     res.status(200).json({
       success: true,
